e80dc66d6ac19a28226d9bdcb7faf1165c0fc4c5
zyxialing <1039574335@qq.com>
2022-01-20 09:11:50 +0800

    mainpanel

--- Assets/Game/Scripts/UI/Panel/Main/Scoller/Cell/MP_MenuItemCell.cs
+++ Assets/Game/Scripts/UI/Panel/Main/Scoller/Cell/MP_MenuItemCell.cs
@@ -1,13 +1,28 @@
 using EnhancedUI.EnhancedScroller;
+using System;
+using UnityEngine;
 using UnityEngine.UI;
 
 public partial class MP_MenuItemCell : EnhancedScrollerCellView
 {
+    private bool _init = false;
     public override void RefreshCellView()
     {
-        AutoInit();
+        Init();
         UIUtils.SetSprite(icon, "icon/tab" + dataIndex);
-        cellBtn.onClick.RemoveAllListeners();
-        cellBtn.onClick.AddListener(() => { ZLogUtil.LogError(dataIndex); });
+
     }
+
+
+    private void Init()
+    {
+        if (!_init)
+        {
+            _init = true;
+            AutoInit();
+            cellBtn.onClick.RemoveAllListeners();
+            cellBtn.onClick.AddListener(() => { ZLogUtil.LogError(dataIndex); });
+        }
+    }
+
 }
--- Assets/Game/Scripts/UI/Panel/Main/Scoller/MP_TapMenu.cs
+++ Assets/Game/Scripts/UI/Panel/Main/Scoller/MP_TapMenu.cs
@@ -1,19 +1,52 @@
 using EnhancedUI.EnhancedScroller;
 using UnityEngine;
 using System;
+using System.Collections;
+using ZFramework;
+using UnityEngine.EventSystems;
 
 [RequireComponent(typeof(EnhancedScroller))]
 public partial class MP_TapMenu : MonoBehaviour, IEnhancedScrollerDelegate
 {
+    private RectTransform scrollRectTransform;
+    private Coroutine _coroutine;
+    private float[] IndexPos;
     public void Init()
     {
         scroller.ReloadData();
+        scrollRectTransform = scroller.gameObject.GetComponent<RectTransform>();
+        IndexPos = new float[6];
+        float width = ((scroller.Container.rect.width - scroller.padding.left - scroller.padding.right) - scroller.spacing / 2f)/(6-1);
+        for (int i = 0; i < 6; i++)
+        {
+            IndexPos[i] = i * width;
+        }
+        scroller.scrollOnBeginDrag = OnBeginDrag;
+        scroller.scrollOnEndDrag = OnEndDrag;
     }
+    private void OnBeginDrag(PointerEventData data)
+    {
+        if (_coroutine != null)
+        {
+            StopCoroutine(_coroutine);
+        }
+    }
+    private void OnEndDrag(PointerEventData data)
+    {
+        if (_coroutine != null)
+        {
+            StopCoroutine(_coroutine);
+        }
+       _coroutine = StartCoroutine(Snap());
+    }
+
+
     public EnhancedScrollerCellView GetCellView(EnhancedScroller scroller, int dataIndex, int cellIndex)
     {
        EnhancedScrollerCellView cellView = scroller.GetCellView(cellViewPrefab);
         cellView.dataIndex = dataIndex;
         cellView.cellIndex = cellIndex;
+        cellView.scroller = scroller;
         cellView.RefreshCellView();
         return cellView;
     }
@@ -27,5 +60,51 @@ public partial class MP_TapMenu : MonoBehaviour, IEnhancedScrollerDelegate
     {
         return 6;
     }
+    #region ¶¯»­
+    private IEnumerator Snap()
+    {
+        while (Mathf.Abs(scroller.LinearVelocity) > scroller.snapVelocityThreshold)
+        {
+            yield return null;
+        }
+        int index = GetSnapIndex(scroller.ScrollPosition);
+        scroller.LinearVelocity = 0;
+        float time = 0.5f;
+        float _tweenTimeLeft = 0;
+        var newPosition = 0f;
+        while (_tweenTimeLeft < time)
+        {
+            newPosition = ZMathUtil.easeInSine(scroller.ScrollPosition, IndexPos[index], (_tweenTimeLeft / time));
 
+            // set the scroll position to the tweened position
+            scroller.ScrollPosition = newPosition;
+
+            // increase the time elapsed
+            _tweenTimeLeft += Time.unscaledDeltaTime;
+
+            yield return null;
+        }
+    }
+
+    private int GetSnapIndex(float curPos)
+    {
+        float dis = float.MaxValue;
+        float tempDis;
+        float tempPos;
+        int index = 0;
+        for (int i = 0; i < IndexPos.Length; i++)
+        {
+
+            tempDis = Mathf.Abs(curPos - IndexPos[i]);
+            if (tempDis < dis)
+            {
+                dis = tempDis;
+                tempPos = IndexPos[i];
+                index = i;
+            }
+        }
+        return index;
+    }
+
+    #endregion
 }
